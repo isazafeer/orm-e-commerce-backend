2a8d50f25413c2d3835db8e700c74d68e07b1e11
Your Name <you@example.com>
2024-04-22 13:22:13 +0100

    catagory.js and product.js updated

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,38 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
-
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
-
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+  },
   },
   {
     sequelize,
@@ -19,5 +43,4 @@ Product.init(
     modelName: 'product',
   }
 );
-
 module.exports = Product;
